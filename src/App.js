89d3a6f33a167467df225c33e31ede10d3015d96
Andrew Formosa <andyformosa@aol.com>
2022-10-12 20:16:44 +0100

    1

--- src/App.js
+++ src/App.js
@@ -1,13 +1,112 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './App.scss';
+import $ from 'jquery'; 
+
+
+//$(document).ready(function(){
+//  $(".expand-button").focus(function(){
+//    $(".text-area").css("height", "90vh");
+//  });
+//  $("button").blur(function(){
+//    $(".text-area").css("height", "30vh");
+//  });
+//});
+
+
+
+
+class TitleBar extends React.Component{
+  constructor(props){
+    super(props);
+    this.state={textheight:""};
+   
+
+  }
+ 
+ //function to expand this area and hide other text areas 
+expandArea(){
+   $(".title-bar .title-icon-shrink").css("display","inline-block");
+    $(".title-bar .title-icon-expand").css("display","none")
+
+    $(".expandable").css("display","none");
+    $(this.props.targetElement).css("display", "inline-block");
+    let textAreaEllement=this.props.targetElement+" .text-area";
+    $(textAreaEllement).css("height", "100vh");
+    this.setState({textheight:$(textAreaEllement.css("height"))});
+  };
+
+
+  shrinkArea(){
+    $(".title-bar .title-icon-shrink").css("display","none");
+     $(".title-bar .title-icon-expand").css("display","inline-block")
+     $(".expandable").css("display","inline-block");
+     let textAreaEllement=this.props.targetElement+" .text-area";
+     $(textAreaEllement).css("height", this.state.textheight);
+   };
+ 
+
+  render(){
+    return(
+      <div className="title-bar">
+      <div className="title-heading">
+       {this.props.title}
+      </div>
+      <div className="title-icon-expand"><button  onClick={()=>{this.expandArea();;}} className="expand-button">X</button></div>
+      <div className="title-icon-shrink"><button onClick={()=>{this.shrinkArea();;}}>S</button></div>
+      </div>
+    );
+  }
+}
+
+
+
+class Editor extends React.Component{
+
+  render(){
+    return(
+      <div className="editor expandable">
+       <TitleBar targetElement={".editor"} title={"EDITOR"} />
+       <div className="mb-2">
+  <textarea className="form-control text-area editor-area" id="exampleFormControlTextarea1" rows="3"></textarea>
+</div>
+        </div>
+    )
+  }
+}
+
+
+class Previewer extends React.Component{
+
+  render(){
+    return(
+      <div className="previewer expandable">
+       <TitleBar targetElement={".previewer"} title={"PREVIEWER"}/>
+       <div className="mb-2">
+  <textarea className="form-control text-area previewer-area" id="exampleFormControlTextarea1" rows="3"></textarea>
+</div>
+        </div>
+    )
+  }
+}
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className="MyApp container">
+      <div className="row">
+      <Editor/>
+      first
+      </div>
+      <div className='row'>
+      <Previewer/>
+
+        secod
+      </div>
+
         <p>
-          Edit <code>src/App.js</code> and save it to reload.
+          Edi <code>src/App.js</code> and save it to reload.
         </p>
         <a
           className="App-link"
@@ -17,7 +116,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+
     </div>
   );
 }

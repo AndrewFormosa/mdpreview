bc2fabcf5f2d09b64a29f5926e2ba5697e224263
Andrew Formosa <andyformosa@aol.com>
2022-10-14 18:52:37 +0100

    1

--- src/App.js
+++ src/App.js
@@ -2,15 +2,60 @@ import React from 'react';
 import './App.scss';
 import $ from 'jquery'; 
 import {marked} from 'marked';
-import DOMPurify from 'dompurify';
-
-import sanitizeHtml from 'sanitize-html';
-import Markdown from 'marked-react';
-import { newObjectInRealm } from 'jsdom/lib/jsdom/living/generated/utils';
+//import Markdown from 'marked-react';
+//import { setNodeSourceCodeLocation } from 'jsdom/lib/jsdom/living/domparsing/parse5-adapter-serialization';
+import OpenInFullIcon from '@mui/icons-material/OpenInFull';
+import CloseFullscreenIcon from '@mui/icons-material/CloseFullscreen';
 
 marked.options({
   breaks:true
 })
+
+let initText=`# Welcome to my React Markdown Previewer!
+
+## This is a sub-heading...
+### And here's some other cool stuff:
+
+Heres some code, `+"`<div></div>`"+`, between 2 backticks.
+
+`+"```"+`
+// this is multi-line code:
+
+function anotherExample(firstLine, lastLine) {
+  if (firstLine == '`+"```' && lastLine == '```')"+` {
+    return multiLineCode;
+  }
+}
+`+"```"+`
+
+You can also make text **bold**... whoa!
+Or _italic_.
+Or... wait for it... **_both!_**
+And feel free to go crazy ~~crossing stuff out~~.
+
+There's also [links](https://www.freecodecamp.org), and
+> Block Quotes!
+
+And if you want to get really crazy, even tables:
+
+Wild Header | Crazy Header | Another Header?
+------------ | ------------- | -------------
+Your content can | be here, and it | can be here....
+And here. | Okay. | I think we get it.
+
+- And of course there are lists.
+  - Some are bulleted.
+     - With different indentation levels.
+        - That look like this.
+
+
+1. And there are numbered lists too.
+1. Use just 1s if you want!
+1. And last but not least, let's not forget embedded images:
+
+![freeCodeCamp Logo](https://cdn.freecodecamp.org/testable-projects-fcc/images/fcc_secondary.svg)
+`
+
 
 
 //$(document).ready(function(){
@@ -39,8 +84,10 @@ expandArea(){
     $(".expandable").css("display","none");
     $(this.props.targetElement).css("display", "inline-block");
     let textAreaEllement=this.props.targetElement+" .text-area";
-    $(textAreaEllement).css("height", "100vh");
+    $(textAreaEllement).css("height", "100%");
+    $(textAreaEllement).css("min-height","80vh")
     this.setState({textheight:$(textAreaEllement.css("height"))});
+
   };
 
 
@@ -50,6 +97,7 @@ expandArea(){
      $(".expandable").css("display","inline-block");
      let textAreaEllement=this.props.targetElement+" .text-area";
      $(textAreaEllement).css("height", this.state.textheight);
+     $(textAreaEllement).css("min-height","30vh");
    };
  
 
@@ -59,8 +107,8 @@ expandArea(){
       <div className="title-heading">
        {this.props.title}
       </div>
-      <div className="title-icon-expand"><button  onClick={()=>{this.expandArea();;}} className="expand-button">X</button></div>
-      <div className="title-icon-shrink"><button onClick={()=>{this.shrinkArea();;}}>S</button></div>
+      <div className="title-icon-expand"><button  onClick={()=>{this.expandArea();;}} className="expand-button"><OpenInFullIcon/></button></div>
+      <div className="title-icon-shrink"><button onClick={()=>{this.shrinkArea();;}}><CloseFullscreenIcon/></button></div>
       </div>
     );
   }
@@ -68,11 +116,11 @@ expandArea(){
 
 
 
+
 class Editor extends React.Component{
 constructor(props){
-  super(props)
-}
-
+  super(props);
+  this.state={initialText:initText};}
 
 
 
@@ -81,7 +129,7 @@ constructor(props){
       <div className="editor expandable">
        <TitleBar targetElement={".editor"} title={"EDITOR"} />
        <div className="mb-2">
-  <textarea onChange={()=>this.props.typeCallBack($("#editor").val())} className="form-control text-area editor-area" id="editor" rows="3"></textarea>
+  <textarea onChange={()=>this.props.typeCallBack($("#editor").val())} className="form-control text-area editor-area" id="editor" rows="3">{this.state.initialText}</textarea>
 </div>
         </div>
     )
@@ -96,15 +144,14 @@ class Previewer extends React.Component{
   }
 
 
+
   render(){
     return(
       <div className="previewer expandable">
        <TitleBar targetElement={".previewer"} title={"PREVIEWER"}/>
        <div className="mb-2">
-      <div dangerouslySetInnerHTML={{__html:this.props.previewText}}  id="preview" className="preview-settings" rows="3"/>
-
-        <div/>
-        </div>
+        <div dangerouslySetInnerHTML={{__html:marked.parse(this.props.previewText)}}  id="preview" className="form-control preview-settings text-area" rows="3"></div>
+     </div>
       </div>
         
     )
@@ -114,56 +161,40 @@ class Previewer extends React.Component{
 
 
 
-
-
 class App extends React.Component {
 constructor(props){
   super(props);
-  this.state={text:""};
+  this.state={text:initText};
   this.updateText=this.updateText.bind(this);
 }
 
 
+
 updateText(newText){
   let parsedText=marked.parse(newText);
-  let sanitizedText=<div className="preview-settings" dangerouslySetInnerHTML={{__html:parsedText }}></div>
-  let markedDown=<Markdown>{newText}</Markdown>;
-  let otherSand=DOMPurify.sanitize(marked.parse(newText));
-  this.setState({text:parsedText});
+  this.setState({text:newText});
 
 }
-
-
 
 
 render(){
   return (
     <div className="MyApp container">
+<div className="row">
+  <h1 className="my-heading my-3 text-center">Markdown Previewer</h1>
+</div>
       <div className="row">
       <Editor typeCallBack={(newText)=> this.updateText(newText)}/>
-      first
       </div>
       <div className='row'>
       <Previewer previewText={this.state.text}/>
-      {this.state.text}
-        secod
+      <footer className="my-footer text-center my-3">By Andrew Formosa. Writen using React, JQuery, bootstrap and SASS as a freeCodeCamp project</footer>
       </div>
-
-        <p>
-          Edi <code>src/App.js</code> and save it to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-
     </div>
   );
 }
 }
+
+
 
 export default App;
